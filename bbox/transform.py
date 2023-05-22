719fadb3441e6a0afe075e2e1a30c332871a7fe3
Pandede <steven9609@gmail.com>
2023-05-22 10:27:18 +0800

    Add: transform

--- bbox/transform.py
+++ bbox/transform.py
@@ -1,16 +1,16 @@
 from .bbox import BoundingBox
 
 
-def union(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
+def smallest_enclosing(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
     """
-    Create a bounding box for enclosing the given bounding boxes.
+    Create a bounding box of their smallest enclosing area.
 
     Args:
         bbox1 (BoundingBox): The first bounding box.
         bbox2 (BoundingBox): The second bounding box.
 
     Returns:
-        BoundingBox: The minimum enclosing bounding box.
+        BoundingBox: The smallest enclosing bounding box.
     """
     x1, y1, x2, y2 = bbox1.to_xyxy()
     a1, b1, a2, b2 = bbox2.to_xyxy()
@@ -18,31 +18,4 @@ def union(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
     return BoundingBox.from_xyxy(min(x1, a1), min(y1, b1), max(x2, a2), max(y2, b2))
 
 
-def intersect(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
-    """
-    Create a bounding box of their intersection.
-
-    If there is no intersection, returns a bounding box with XYWH `((bbox1.x + bbox2.x) / 2, (bbox1.y + bbox2.y) / 2), 0, 0`.
-
-    Args:
-        bbox1 (BoundingBox): The first bounding box.
-        bbox2 (BoundingBox): The second bounding box.
-
-    Returns:
-        BoundingBox: The intersecting bounding box.
-    """
-    x1, y1, x2, y2 = bbox1.to_xyxy()
-    a1, b1, a2, b2 = bbox2.to_xyxy()
-
-    # Compute the coordinate of top-left point and bottom-right point of the overlapping
-    overlap_x1 = max(x1, a1)
-    overlap_y1 = max(y1, b1)
-    overlap_x2 = min(x2, a2)
-    overlap_y2 = min(y2, b2)
-
-    if overlap_x2 - overlap_x1 <= 0 or overlap_y2 - overlap_y1 <= 0:
-        cx = (bbox1.x + bbox2.x) // 2
-        cy = (bbox1.y + bbox2.y) // 2
-        return BoundingBox(x=cx, y=cy, w=0, h=0)
-    else:
-        return BoundingBox.from_xyxy(max(x1, a1), max(y1, b1), min(x2, a2), min(y2, b2))
+bound = smallest_enclosing
--- tests/bbox/test_transform.py
+++ tests/bbox/test_transform.py
@@ -3,13 +3,13 @@ from typing import Tuple
 import pytest
 
 from bbox import BoundingBox
-from bbox.transform import union, intersect
+from bbox.transform import bound, smallest_enclosing
 
 XYXY = Tuple[int, int, int, int]
 
 
 @pytest.mark.parametrize(
-    'xyxy1,xyxy2,union_xyxy', (
+    'xyxy1,xyxy2,se_xyxy', (
         ((0, 0, 10, 10), (20, 20, 30, 30), (0, 0, 30, 30)),
         ((0, 0, 10, 10), (0, 0, 10, 10), (0, 0, 10, 10)),
         ((0, 0, 10, 10), (5, 5, 15, 15), (0, 0, 15, 15)),
@@ -17,24 +17,8 @@ XYXY = Tuple[int, int, int, int]
         ((0, 10, 10, 20), (10, 0, 20, 10), (0, 0, 20, 20))
     )
 )
-def test_union(xyxy1: XYXY, xyxy2: XYXY, union_xyxy: XYXY):
+def test_smallest_enclosing(xyxy1: XYXY, xyxy2: XYXY, se_xyxy: XYXY):
     bbox1 = BoundingBox.from_xyxy(*xyxy1)
     bbox2 = BoundingBox.from_xyxy(*xyxy2)
-    union_bbox = BoundingBox.from_xyxy(*union_xyxy)
-    assert union(bbox1, bbox2) == union_bbox
-
-
-@pytest.mark.parametrize(
-    'xyxy1,xyxy2,inter_xyxy', (
-        ((0, 0, 10, 10), (20, 20, 30, 30), (15, 15, 15, 15)),
-        ((0, 0, 10, 10), (0, 0, 10, 10), (0, 0, 10, 10)),
-        ((0, 0, 10, 10), (5, 5, 15, 15), (5, 5, 10, 10)),
-        ((0, 0, 20, 20), (5, 5, 15, 15), (5, 5, 15, 15)),
-        ((0, 10, 10, 20), (10, 0, 20, 10), (10, 10, 10, 10))
-    )
-)
-def test_intersect(xyxy1: XYXY, xyxy2: XYXY, inter_xyxy: XYXY):
-    bbox1 = BoundingBox.from_xyxy(*xyxy1)
-    bbox2 = BoundingBox.from_xyxy(*xyxy2)
-    inter_bbox = BoundingBox.from_xyxy(*inter_xyxy)
-    assert intersect(bbox1, bbox2) == inter_bbox
+    se_bbox = BoundingBox.from_xyxy(*se_xyxy)
+    assert smallest_enclosing(bbox1, bbox2) == bound(bbox1, bbox2) == se_bbox
