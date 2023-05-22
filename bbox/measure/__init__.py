e163ab859dc5fdc41bf910267a6e61672164cb7f
Pandede <steven9609@gmail.com>
2023-05-22 11:19:43 +0800

    Add: shortcut for `diou` and `ciou`

--- bbox/measure/__init__.py
+++ bbox/measure/__init__.py
@@ -1,7 +1,7 @@
 from .area import intersect, union
-from .iou import giou, iou
+from .iou import giou, iou, diou, ciou
 
 __all__ = [
     'intersect', 'union',
-    'iou', 'giou'
+    'iou', 'giou', 'diou', 'ciou'
 ]
