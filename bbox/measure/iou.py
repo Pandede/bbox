8ec48a75fb5ba412a1bc04924b4fe388ab0e636c
Pandede <steven9609@gmail.com>
2023-05-22 11:07:39 +0800

    Add: measure.iou.diou

--- bbox/measure/iou.py
+++ bbox/measure/iou.py
@@ -42,3 +42,26 @@ def giou(bbox1: BoundingBox, bbox2: BoundingBox) -> float:
     score = iou_score - (se_area - union_area) / se_area
 
     return score
+
+
+def diou(bbox1: BoundingBox, bbox2: BoundingBox) -> float:
+    """
+    Compute DIoU score of bounding boxes.
+
+    Args:
+        bbox1 (BoundingBox): The predict bounding box.
+        bbox2 (BoundingBox): The groundtruth bounding box.
+
+    Returns:
+        float: the DIoU score
+    """
+    iou_score = iou(bbox1, bbox2)
+
+    # Compute the L2-distance of the center points
+    center_dist = (bbox1.x - bbox2.x) ** 2 + (bbox1.y - bbox2.y) ** 2
+
+    # Compute the L2-distance of the diagonal points in the smallest enclosing bounding box
+    sx1, sy1, sx2, sy2 = smallest_enclosing(bbox1, bbox2).to_xyxy()
+    se_dist = (sx1 - sx2) ** 2 + (sy1 - sy2) ** 2
+
+    return iou_score - center_dist / se_dist
--- tests/bbox/measure/test_iou.py
+++ tests/bbox/measure/test_iou.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import pytest
 
 from bbox import BoundingBox
-from bbox.measure.iou import giou, iou
+from bbox.measure.iou import giou, iou, diou
 
 XYXY = Tuple[int, int, int, int]
 
@@ -36,3 +36,18 @@ def test_giou(xyxy1: XYXY, xyxy2: XYXY, score: float):
     bbox1 = BoundingBox.from_xyxy(*xyxy1)
     bbox2 = BoundingBox.from_xyxy(*xyxy2)
     assert giou(bbox1, bbox2) == pytest.approx(score)
+
+
+@pytest.mark.parametrize(
+    'xyxy1,xyxy2,score', (
+        ((0, 0, 10, 10), (20, 20, 30, 30), -0.4444444),
+        ((0, 0, 10, 10), (0, 0, 10, 10), 1.0),
+        ((0, 0, 10, 10), (5, 5, 15, 15), 0.01530612),
+        ((0, 0, 20, 20), (5, 5, 15, 15), 0.25),
+        ((0, 0, 10, 10), (10, 10, 20, 20), -0.25)
+    )
+)
+def test_diou(xyxy1: XYXY, xyxy2: XYXY, score: float):
+    bbox1 = BoundingBox.from_xyxy(*xyxy1)
+    bbox2 = BoundingBox.from_xyxy(*xyxy2)
+    assert diou(bbox1, bbox2) == pytest.approx(score)
