c077181a9e7644fe6356ee39cce4ac9a897b841e
Pandede <steven9609@gmail.com>
2023-05-22 18:28:54 +0800

    Add: setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='bbox',
+    version='0.1.0',
+    description='A Python library for handling the 2D bounding box.',
+    author='Pandede',
+    packages=[
+        "pydantic"
+    ]
+)
