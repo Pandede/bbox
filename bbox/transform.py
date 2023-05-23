71ca931405095daffa87098db887da56df621423
Pandede <steven9609@gmail.com>
2023-05-23 12:01:10 +0800

    Add: `transform.scaling` and `transform.scaling_all`

--- bbox/transform.py
+++ bbox/transform.py
@@ -19,3 +19,46 @@ def smallest_enclosing(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
 
 
 bound = smallest_enclosing
+
+
+def scaling(bbox: BoundingBox, top: float = 1.0, bottom: float = 1.0, left: float = 1.0, right: float = 1.0) -> BoundingBox:
+    """
+    Scaling the bounding box along the single direction.
+
+    Args:
+        bbox (BoundingBox): The bounding box to be resized.
+        top (float, optional): The scaling ratio along the top edge. Defaults to 1.0.
+        bottom (float, optional): The scaling ratio along the bottom edge. Defaults to 1.0.
+        left (float, optional): The scaling ratio along the left edge. Defaults to 1.0.
+        right (float, optional): The scaling ratio along the right edge. Defaults to 1.0.
+
+    Returns:
+        BoundingBox: The resized bounding box.
+    """
+    assert top >= 0, 'scale top cannot be negative'
+    assert bottom >= 0, 'scale bottom cannot be negative'
+    assert left >= 0, 'scale left cannot be negative'
+    assert right >= 0, 'scale right cannot be negative'
+
+    dw, dh = bbox.w // 2, bbox.h // 2
+    return BoundingBox.from_xyxy(
+        bbox.x - dw * left,
+        bbox.y - dh * top,
+        bbox.x + dw * right,
+        bbox.y + dh * bottom
+    )
+
+
+def scaling_all(bbox: BoundingBox, scale: float = 1.0) -> BoundingBox:
+    """
+    Scaling the bounding box according to the ratio.
+
+    Args:
+        bbox (BoundingBox): The bounding box to be resized.
+        scale (float, optional): The scaling ratio. Defaults to 1.0.
+
+    Returns:
+        BoundingBox: The resized bounding box.
+    """
+    assert scale >= 0, 'scale cannot be negative'
+    return scaling(bbox, top=scale, bottom=scale, left=scale, right=scale)
--- tests/bbox/test_transform.py
+++ tests/bbox/test_transform.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import pytest
 
 from bbox import BoundingBox
-from bbox.transform import bound, smallest_enclosing
+from bbox.transform import bound, scaling, scaling_all, smallest_enclosing
 
 XYXY = Tuple[int, int, int, int]
 
@@ -22,3 +22,55 @@ def test_smallest_enclosing(xyxy1: XYXY, xyxy2: XYXY, se_xyxy: XYXY):
     bbox2 = BoundingBox.from_xyxy(*xyxy2)
     se_bbox = BoundingBox.from_xyxy(*se_xyxy)
     assert smallest_enclosing(bbox1, bbox2) == bound(bbox1, bbox2) == se_bbox
+
+
+@pytest.mark.parametrize(
+    'xyxy,top,bottom,left,right,scale_xyxy', (
+        ((0, 0, 100, 100), 1.0, 1.0, 1.0, 1.0, (0, 0, 100, 100)),
+        ((0, 0, 100, 100), 1.2, 1.0, 1.0, 1.0, (0, -10, 100, 100)),
+        ((0, 0, 100, 100), 1.0, 1.2, 1.0, 1.0, (0, 0, 100, 110)),
+        ((0, 0, 100, 100), 1.0, 1.0, 1.2, 1.0, (-10, 0, 100, 100)),
+        ((0, 0, 100, 100), 1.0, 1.0, 1.0, 1.2, (0, 0, 110, 100)),
+        ((0, 0, 100, 100), 1.2, 1.2, 1.2, 1.2, (-10, -10, 110, 110)),
+        ((0, 0, 100, 100), 0.8, 1.0, 1.0, 1.0, (0, 10, 100, 100)),
+        ((0, 0, 100, 100), 1.0, 0.8, 1.0, 1.0, (0, 0, 100, 90)),
+        ((0, 0, 100, 100), 1.0, 1.0, 0.8, 1.0, (10, 0, 100, 100)),
+        ((0, 0, 100, 100), 1.0, 1.0, 1.0, 0.8, (0, 0, 90, 100)),
+        ((0, 0, 100, 100), 0.8, 0.8, 0.8, 0.8, (10, 10, 90, 90))
+    )
+)
+def test_scaling(xyxy: XYXY, top: float, bottom: float, left: float, right: float, scale_xyxy: XYXY):
+    bbox = BoundingBox.from_xyxy(*xyxy)
+    scale_bbox = BoundingBox.from_xyxy(*scale_xyxy)
+    assert scaling(bbox, top, bottom, left, right) == scale_bbox
+
+
+def test_scaling_with_negative_scale():
+    bbox = BoundingBox.from_xyxy(0, 0, 100, 100)
+    with pytest.raises(AssertionError, match='scale top cannot be negative'):
+        scaling(bbox, top=-0.1)
+    with pytest.raises(AssertionError, match='scale bottom cannot be negative'):
+        scaling(bbox, bottom=-0.1)
+    with pytest.raises(AssertionError, match='scale left cannot be negative'):
+        scaling(bbox, left=-0.1)
+    with pytest.raises(AssertionError, match='scale right cannot be negative'):
+        scaling(bbox, right=-0.1)
+
+
+@pytest.mark.parametrize(
+    'xyxy,scale,scale_xyxy', (
+        ((0, 0, 100, 100), 1.0, (0, 0, 100, 100)),
+        ((0, 0, 100, 100), 1.2, (-10, -10, 110, 110)),
+        ((0, 0, 100, 100), 0.8, (10, 10, 90, 90)),
+    )
+)
+def test_scaling_all(xyxy: XYXY, scale: float, scale_xyxy: XYXY):
+    bbox = BoundingBox.from_xyxy(*xyxy)
+    scale_bbox = BoundingBox.from_xyxy(*scale_xyxy)
+    assert scaling_all(bbox, scale) == scale_bbox
+
+
+def test_scaling_all_with_negative_scale():
+    bbox = BoundingBox.from_xyxy(0, 0, 100, 100)
+    with pytest.raises(AssertionError, match='scale cannot be negative'):
+        scaling_all(bbox, scale=-0.1)
