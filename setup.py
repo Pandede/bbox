c2a145f708278f4513550fab5ad080ccae72a8fd
Pandede <steven9609@gmail.com>
2023-05-23 14:33:06 +0800

    Fix: add missing args `packages`

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bbox',
     version='0.1.0',
     description='A Python library for handling the 2D bounding box.',
     author='Pandede',
+    packages=find_packages(),
     install_requires=[
         "pydantic"
     ]
